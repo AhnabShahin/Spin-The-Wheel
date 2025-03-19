4f7ff5ddc744b7248e93bc1125bc9926e655c7da
Ahnab Shahin <shahinahnab@gmail.com>
2025-03-19 15:28:11 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -14,24 +14,5 @@ export default defineConfig({
         assetFileNames: 'assets/[name].[ext]'
       }
     }
-  },
-  server: {
-    port: 3000,
-    cors: true,
-    strictPort: true,
-    hmr: {
-      port: 3000,
-      host: 'wordpress.test',
-      protocol: 'ws',
-      overlay: true,
-      clientPort: 3000
-    },
-    watch: {
-      usePolling: true,
-      interval: 1000
-    }
-  },
-  base: process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000/'
-    : '/wp-content/plugins/Spin-The-Wheel/dist/'
+  }
 })
