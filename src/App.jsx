251a48ec35421a2f1b4c0eaa3594476682462c7e
Ahnab Shahin <shahinahnab@gmail.com>
2025-03-19 15:46:38 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ThemeList from './Theme/List/ThemeList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,14 +10,7 @@ function App() {
   return (
     <div className="spin-wheel-admin-container">
       <h1>Spin The Wheel Settings</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Test Resda t::  {count}
-        </button>
-        <p>
-          If you can see this and the counter works, React is properly set up!
-        </p>
-      </div>
+      <ThemeList />
     </div>
   )
 }
